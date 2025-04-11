eacbe2f813bb27df5af6a4bfe999ee79b81491d0
sunjaelee2 <sj.lee2@fanmaum.com>
2025-04-11 16:43:06 +0900

    cell 테스트 작성완료

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 
-interface CellProps {
+export interface CellProps {
   row: number;
   column: number;
   isOpen: boolean;
+  isFlag: boolean;
+  isMine: boolean;
+  mineCount: number;
   onClick: (params: { row: number; column: number }) => void;
+  onClickContext: (params: { row: number; column: number }) => void;
 }
 
-export const Cell = ({ row, column, isOpen, onClick }: CellProps) => {
+export const Cell = ({
+  row,
+  column,
+  isOpen,
+  isFlag,
+  isMine,
+  mineCount,
+  onClick,
+  onClickContext,
+}: CellProps) => {
+  const getButtonText = () => {
+    if (isOpen === false) return isFlag ? "🚩" : "";
+    if (isMine) return "💣";
+    return mineCount ? mineCount : "";
+  };
+
   return (
-    <button onClick={() => onClick({ row, column })} disabled={isOpen}></button>
+    <button
+      onClick={() => onClick({ row, column })}
+      onContextMenu={() => {
+        if (isOpen) return;
+        onClickContext({ row, column });
+      }}
+      disabled={isOpen || isFlag}
+    >
+      {getButtonText()}
+    </button>
   );
 };
