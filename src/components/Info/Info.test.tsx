2a769be6b73c0c4d2d6b84d88b44fd761adef759
SeonjaeLee <tjswo9502@gmail.com>
2025-04-13 14:20:35 +0900

    info 테스트 작성 완료

--- src/components/Info/Info.test.tsx
+++ src/components/Info/Info.test.tsx
@@ -1,5 +1,46 @@
-import { test } from "vitest";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import { afterEach, expect, test, vi } from "vitest";
+import { Info, InfoProps } from "./Info";
 
-test.todo("현재 지뢰 개수를 표시한다.");
+test("현재 지뢰 개수를 표시한다.", () => {
+  const cellList: InfoProps["cellList"] = Array.from({ length: 3 }, (_, row) =>
+    Array.from({ length: 3 }, (_, column) => ({
+      row,
+      column,
+      isOpen: false,
+      isFlag: false,
+      isMine: row === 0,
+      mineCount: 0,
+    }))
+  );
+  const onClickSpy = vi.fn();
+  render(<Info cellList={cellList} onReset={onClickSpy} />);
 
-test.todo("지뢰 개수 변경된다.");
+  const mineCounter = screen.getByRole("region", { name: "mineCounter" });
+
+  expect(mineCounter.textContent).toBe("3");
+});
+
+test("게임이 초기화 된다.", () => {
+  const cellList: InfoProps["cellList"] = Array.from({ length: 3 }, (_, row) =>
+    Array.from({ length: 3 }, (_, column) => ({
+      row,
+      column,
+      isOpen: false,
+      isFlag: false,
+      isMine: row === 0,
+      mineCount: 0,
+    }))
+  );
+  const onClickSpy = vi.fn();
+  render(<Info cellList={cellList} onReset={onClickSpy} />);
+  const resetButton = screen.getByRole("button", { name: "reset" });
+
+  fireEvent.click(resetButton);
+
+  expect(onClickSpy).toHaveBeenCalledOnce();
+});
+
+afterEach(() => {
+  cleanup();
+});
