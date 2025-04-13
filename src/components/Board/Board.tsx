b885f4022630ee83058026c11d9422c5f767a00c
SeonjaeLee <tjswo9502@gmail.com>
2025-04-13 13:36:32 +0900

    board 테스트 작성완료

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Cell } from "../Cell/Cell";
+import { Cell, CellProps } from "../Cell/Cell";
 
-export const Board = () => {
+export interface BoardProps {
+  cellList: Omit<CellProps, "onClick" | "onClickContext">[][];
+  onClick: CellProps["onClick"];
+  onContextClick: CellProps["onClickContext"];
+}
+
+export const Board = ({ cellList, onClick, onContextClick }: BoardProps) => {
   return (
-    <div>
-      <Cell />
+    <div role="region" aria-label="board">
+      {cellList.map((row, i) => (
+        <ul key={`row-${i}`}>
+          {row.map((cell, j) => (
+            <li key={`cell-${i}-${j}`}>
+              <Cell
+                {...cell}
+                onClick={onClick}
+                onClickContext={onContextClick}
+              />
+            </li>
+          ))}
+        </ul>
+      ))}
     </div>
   );
 };
