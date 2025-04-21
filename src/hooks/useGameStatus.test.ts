aa2939508b769b16d72dea6d4c282e315aa1a9d9
이선재 <tjswo9502@gmail.com>
2025-04-22 08:22:12 +0900

    초기 테스트 코드 기반으로 ai 구현

---
+++ src/hooks/useGameStatus.test.ts
@@ -0,0 +1,10 @@
+import { test } from "vitest";
+
+test.todo("지뢰개수와 N * M 2차원 배열을 받아 랜덤으로 생성한다");
+test.todo("지뢰가 중복되지 않도록 무작위 위치에 지정된 개수만큼 생성된다.");
+test.todo("각 셀의 mineCount는 주변 8칸의 지뢰 개수를 정확히 나타낸다.");
+
+test.todo("깃발을 토글하면 isFlag 상태가 true로 바뀐다.");
+test.todo("이미 깃발이 있는 셀에 토글하면 isFlag가 false로 바뀐다.");
+test.todo("열린 셀에 깃발을 토글할 수 없다.");
+test.todo("깃발이 있는 셀은 열리지 않는다.");
